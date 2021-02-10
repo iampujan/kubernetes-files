e8ca8ab8915a6c53ca0e86414864206b9771d154
Ubuntu <azureuser@k8-machine.nchbwaw3acluzjzt1knklrgihh.bx.internal.cloudapp.net>
2021-02-10 16:10:27 +0000

    asdasdasd

---
+++ main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install docker docker.io apt-transport-https virtualbox virtualbox-ext-pack -y
+sudo systemctl start docker
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod 755 /usr/local/bin/minikube
+minikube version
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version -o json
+minikube start
+
--- myApplication/starter.sh
+++ myApplication/starter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 minikube addons enable ingress
+sudo cp hosts /etc/hosts
 sleep 2
 kubectl create -f $(ls -1 *.yaml | xargs | sed -e 's/ /,/g')
 
